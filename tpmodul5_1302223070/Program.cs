b2c3f1441d6838d95f5c900c4ba0e197fb459e3e
GinaSoraya <ginasry08@gmail.com>
2024-03-17 11:46:00 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223070/Program.cs
+++ tpmodul5_1302223070/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class HaloGeneric
+{
+    private static void Main(string[] args)
+    {
+        DataGeneric<string> input1 = new DataGeneric<string>("1302223070");
+        input1.PrintData();
+    }
+    class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T input)
+        {
+            this.data = input;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+}
